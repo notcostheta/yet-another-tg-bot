ab823a549e8aa423e202fef52fa6431cad146b44
notcostheta <kaustubhbhiwsankar@gmail.com>
2024-01-28 09:19:01 +0530

    the caching logic works now, will work on the cleanup later since i need to look at this code as a refernce later

--- bot/plugins/test/getid.py
+++ bot/plugins/test/getid.py
@@ -4,7 +4,9 @@ from pyrogram.types import Message
 from bot.helpers.decorators import ratelimiter
 from bot.configs.config import CACHE_CHANNEL
 
-channel = int(CACHE_CHANNEL)
+channel = int(CACHE_CHANNEL)  # Important to convert to int
+
+
 @Client.on_message(filters.command("id"))
 @ratelimiter
 async def get_id(client: Client, message: Message):
@@ -19,8 +21,24 @@ async def upload(client: Client, message: Message):
     if len(message.command) > 1:
         query = message.text.split(" ", 1)[1]
         dl = await message.reply_text("Downloading...")
-        channel_info = await client.get_chat(channel)
-        print(channel_info)
+
+        req = await client.send_message(
+            chat_id=channel,
+            text=f"{query} requested by {message.from_user.mention}",
+            disable_notification=True,
+        )
+
+        audio_path = "content/Catalyst... - Nice to see you.mp3"
+        file = await client.send_audio(
+            chat_id=channel,
+            audio=audio_path,
+        )
+
+        file_id = file.audio.file_id
+        send = await client.send_cached_media(chat_id=message.chat.id, file_id=file_id)
+
+        # channel_info = await client.get_chat(channel)
+        # print(channel_info)
         return
     else:
         await message.reply_text("Provide a valid URL")
